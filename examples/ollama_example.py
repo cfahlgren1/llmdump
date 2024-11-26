cd3a6809141e8cab84acc34f17dc0d035ba71b91
Jae Saxon <jaesaxon@honeycomb.io>
2024-11-25 18:08:17 -0600

    update ollama example to work even if there is no openai api key present

--- examples/ollama_example.py
+++ examples/ollama_example.py
@@ -7,6 +7,9 @@ store = DuckDBStore()
 # Ollama is running locally at http://localhost:11434/v1
 openai_client = OpenAI(
     base_url="http://localhost:11434/v1",
+    # The openai client needs an api key to instantiate, but we
+    # are using this to access ollama, which does not use it
+    api_key='unused'
 )
 
 client = wrap_openai(openai_client, store=store)
