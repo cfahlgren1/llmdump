4480090db64a03774a6c25366a461ff8fb73a24e
Jae Saxon <jaesaxon@honeycomb.io>
2024-11-27 10:27:20 -0600

    remove api-key comment and disabler

--- examples/observers/ollama_example.py
+++ examples/observers/ollama_example.py
@@ -2,7 +2,9 @@ from observers.observers import wrap_openai
 from openai import OpenAI
 
 # Ollama is running locally at http://localhost:11434/v1
-openai_client = OpenAI(base_url="http://localhost:11434/v1")
+openai_client = OpenAI(
+    base_url="http://localhost:11434/v1",
+)
 
 client = wrap_openai(openai_client)
 
