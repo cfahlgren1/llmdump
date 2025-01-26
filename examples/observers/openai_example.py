0bd033cd02cc465943dd66624f88228b0858c598
Caleb Fahlgren <cfahlgren1@gmail.com>
2025-01-26 19:32:46 +0100

    update example

--- examples/observers/openai_example.py
+++ examples/observers/openai_example.py
@@ -1,18 +1,14 @@
-import os
-
 from openai import OpenAI
 
 from observers import wrap_openai
 
 
-openai_client = OpenAI(
-    base_url="https://api-inference.huggingface.co/v1/", api_key=os.getenv("HF_TOKEN")
-)
+openai_client = OpenAI()
 
 client = wrap_openai(openai_client)
 
 response = client.chat.completions.create(
-    model="Qwen/Qwen2.5-72B-Instruct",
+    model="gpt-4o",
     messages=[{"role": "user", "content": "Tell me a joke."}],
 )
 print(response)
