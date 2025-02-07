6768030c925740def7bb09960aa0600f57165caf
Caleb Fahlgren <cfahlgren1@gmail.com>
2025-02-07 10:20:38 +0100

    update openai example

--- examples/models/openai_example.py
+++ examples/models/openai_example.py
@@ -10,10 +10,7 @@ client = wrap_openai(openai_client)
 response = client.chat.completions.create(
     model="gpt-4o",
     messages=[{"role": "user", "content": "Tell me a joke in the voice of a pirate."}],
-    stream=True,
     temperature=0.5,
 )
 
-for chunk in response:
-    if chunk.choices[0].delta.content:
-        print(chunk.choices[0].delta.content, end="", flush=True)
+print(response.choices[0].message.content)
